5caec94f1b0d39f428503de944d9f1ae07e8af9a
Npelletier91 <nicolas.pelletier@yahoo.com>
2023-08-13 11:53:46 -0400

    Add project files.

---
+++ TestSiteVisualStudio/wwwroot/js/site.js
@@ -0,0 +1,14 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your Javascript code.
+
+const move = document.getElementById('move');
+
+move.addEventListener('click', () => {
+
+    const randomX = Math.floor(Math.random() * 100);
+    const randomY = Math.floor(Math.random() * 100);
+
+    move.style.transform = `translate(${randomX}px, ${randomY}px)`;
+});
